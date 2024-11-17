7066f17e262bf75768b072b31d89ec6fb933dde7
edudilascio <164105796+edudilasci0@users.noreply.github.com>
2024-11-17 13:05:08 -0300

    Update wallets_manager.py

--- wallets_manager.py
+++ wallets_manager.py
@@ -1,5 +1,22 @@
 def load_wallets():
+    """
+    Devuelve la lista de wallets a monitorear con etiquetas descriptivas.
+    """
     return [
-        {"tag": "Wallet1", "address": "HUpPyLU8KWisCAr3mzWy2FKT6uuxQ2qGgJQxyTpDoes5"},
-        {"tag": "Wallet2", "address": "71CPXu3TvH3iUKaY1bNkAAow24k6tjH473SsKprQBABC"},
+        {"tag": "0xsun", "address": "HUpPyLU8KWisCAr3mzWy2FKT6uuxQ2qGgJQxyTpDoes5"},
+        {"tag": "KoreanGenius", "address": "71CPXu3TvH3iUKaY1bNkAAow24k6tjH473SsKprQBABC"},
+        {"tag": "0xcryptoyang", "address": "2sBfRsYScANaXjUC8FvSn5vaeNALADf63iqXdyKEJjFP"},
+        {"tag": "SMpumper1", "address": "CYWHDsprQJZu2e6umFAVCAHRK4z4KJWYNhSGWtTEsBve"},
+        {"tag": "SMpumper2", "address": "72e6QM7gn9MH5u1YpgQbduexm4WAon1cnsSXPqKnLQec"},
+        {"tag": "SMpumper3", "address": "FYGgfgZFeVxnJKF2RS6MKYHBsUpfJdCwumzkPpxWPM4u"},
+        {"tag": "0xdutufarmer", "address": "5UWoHLhq112DCJ3jv8vayZPZbZTyBhXrWd121mYHjYU1"},
+        {"tag": "Degen1", "address": "DyzM5wENV6YwvC5umXYjGxb1WCE4FfZy11bc7A3EsXLJ"},
+        {"tag": "SMpumper4", "address": "2byqB8Pypvq6s8W126yJc9SxToTiWTfNFuDU3ZTq9CLT"},
+        {"tag": "Sniper", "address": "39aSEwzyErYwEKs257TDzonPEVwWSRuj7SNMMBrGCAmz"},
+        {"tag": "OxuezhangKOL", "address": "Gf9XgdmvNHt8fUTFsWAccNbKeyDXsgJyZN8iFJKg5Pbd"},
+        {"tag": "SniperHighrisk", "address": "4hBL4Z2Tvn2bCNqZniAxL82xviPJaTQeyKMdnLwsVt7L"},
+        {"tag": "Degen2", "address": "EJ7SM8HESPBt6m5nmBFajQMYCH1bov6i7WcaTHodfNpd"},
+        {"tag": "Sniper", "address": "5zMn2ATK4rbECAcbE4y6A8hg93BpsmSNJ2jpiBgoXaMr"},
+        {"tag": "Whale 3", "address": "9UWZFoiCHeYRLmzmDJhdMrP7wgrTw7DMSpPiT2eHgJHe"},
+        {"tag": "Hacker", "address": "68ou4g8ATZsC8XWC9NcYv2UHMdp3NtxRsfZwuGgDhUyp"}
     ]
