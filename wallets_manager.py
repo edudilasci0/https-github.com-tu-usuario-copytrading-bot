3681e49b05b6cbe0f01ff077d87394a070e0ed39
edudilascio <164105796+edudilasci0@users.noreply.github.com>
2024-11-17 12:42:45 -0300

    Create wallets_manager.py

---
+++ wallets_manager.py
@@ -0,0 +1,5 @@
+def load_wallets():
+    return [
+        {"tag": "Wallet1", "address": "HUpPyLU8KWisCAr3mzWy2FKT6uuxQ2qGgJQxyTpDoes5"},
+        {"tag": "Wallet2", "address": "71CPXu3TvH3iUKaY1bNkAAow24k6tjH473SsKprQBABC"},
+    ]
